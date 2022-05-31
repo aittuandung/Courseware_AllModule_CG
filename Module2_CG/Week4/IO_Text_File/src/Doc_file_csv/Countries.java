7985929a4d556da593b7707b0d543c9190db2cd7
aittuandung <tuandung.ait.it@gmail.com>
2022-05-31 12:37:58 +0700

    add

---
+++ Module2_CG/Week4/IO_Text_File/src/Doc_file_csv/Countries.java
@@ -0,0 +1,2 @@
+package Doc_file_csv;public class Countries {
+}
---
+++ Module2_CG/Week4/QuanLyCodeGym/src/Main.java
@@ -0,0 +1,12 @@
+package models;
+
+import Controllers.ManagerStudent;
+
+public class Main {
+    public static void main(String[] args) {
+        ManagerStudent managerStudent = new ManagerStudent();
+        while (true){
+            managerStudent.menu();
+        }
+    }
+}
